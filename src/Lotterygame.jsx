8d8ed5555bdf53714c52931db17c139bf378f19d
mayankchugh17 <mayankchugh222@gmail.com>
2025-04-09 23:17:41 +0530

    make sum function

--- src/Lotterygame.jsx
+++ src/Lotterygame.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
-import { genTicket } from "./helper";
+import { genTicket,sum } from "./helper";
 import "./lottery.css";
 
 export default function LotteryGame(){
     const [ticket, setTicket] = useState(genTicket(3));
-    return (
+    let isWinning = sum(ticket) === 15;
+
+    let buyTicket = () =>{
+        setTicket(genTicket(3))
+    }
+        return (
         <>
             <h1>Lottery Game!</h1>
             <div className="ticket">
@@ -12,6 +17,9 @@ export default function LotteryGame(){
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <br />
+            <button onClick={buyTicket}>Buy New Ticket</button>
+            <h3>{isWinning && "Congratulations, you won!"}</h3>
         </>
     );
 }
