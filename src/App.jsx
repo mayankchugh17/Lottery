edaae1ba29018dae01b84b6f0123736174cd1a31
mayankchugh17 <mayankchugh222@gmail.com>
2025-04-12 11:43:12 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import LotteryGame from './Lottery.jsx'
+import Lottery from './Lottery.jsx'
 import Ticket from './Ticket.jsx'
 
 import './App.css'
 
 function App() {
+  
+  let winningCondition = (ticket) =>{
+    return ticket.every((num) => {num === ticket[0]});      //All 3 TicketNumbers should be same to get win.
+  };
+
   return (
    <>
-     <LotteryGame />
+     <Lottery n={3} winningCondition={winningCondition} />
    </>
   )
 }
--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { genTicket,sum } from "./helper.js";
 import Ticket from "./Ticket.jsx";
 
-export default function LotteryGame({ n = 3, winningSum = 10}){
+export default function Lottery({ n = 3, winningCondition}){        //winningCondition (Function as Probs)
     const [ticket, setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) === winningSum;
+    let isWinning = winningCondition(ticket);
 
     let buyTicket = () =>{
         setTicket(genTicket(n))
